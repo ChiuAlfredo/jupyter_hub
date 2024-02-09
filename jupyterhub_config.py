cbff101ca74223ca532dcf6ab50357b3380e5f5c
alfredochiu <petercy32@gmail.com>
2024-02-09 20:58:11 +0800

    update

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -6,10 +6,14 @@ c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 
 c.DockerSpawner.allowed_images = {
-    "tensorflow-cpu": "jupyter/tensorflow-notebook",
-    "tensorflow-gpu (Tensorflow 2.8)": "harbor.aif.tw/env/tensorflow-notebook",
-    "pytorch-gpu (Pytorch 1.10)": "harbor.aif.tw/env/pytorch-notebook"
+    "tensorflow-cpu": "tensorflow-notebook",
+    "tensorflow-gpu (Tensorflow 2.8)": "tensorflow-notebook"
 }
+# c.DockerSpawner.allowed_images = {
+#     "tensorflow-cpu": "tensorflow-notebook",
+#     "tensorflow-gpu (Tensorflow 2.8)": "tensorflow-notebook",
+#     "pytorch-gpu (Pytorch 1.10)": "pytorch-notebook"
+# }
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.extra_host_config = {'runtime': 'nvidia'}
 c.Spawner.environment = {'GRANT_SUDO': 'yes'}
