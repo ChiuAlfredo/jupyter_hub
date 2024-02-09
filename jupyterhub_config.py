26042d978de771aad05b0b07c0c4f974e3383c59
alfredochiu <petercy32@gmail.com>
2024-02-09 20:45:59 +0800

    update

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -16,9 +16,9 @@ c.Spawner.environment = {'GRANT_SUDO': 'yes'}
 c.Spawner.default_url = '/lab'
 
 
-###
-指定admin為管理者帳號；設定使用FirstUseAuthenticator，我們直接使用內建的帳號管理系統，從admin介面新增使用者之後，使用者在第一次登入時必須自行設定密碼。
-###
+
+#指定admin為管理者帳號；設定使用FirstUseAuthenticator，我們直接使用內建的帳號管理系統，從admin介面新增使用者之後，使用者在第一次登入時必須自行設定密碼。
+
 from jupyter_client.localinterfaces import public_ips
 c.JupyterHub.hub_ip = public_ips()[0]
 c.JupyterHub.admin_access = True
@@ -27,9 +27,9 @@ c.Authenticator.admin_users = {'admin'}
 c.JupyterHub.authenticator_class = 'firstuseauthenticator.FirstUseAuthenticator'
 c.LocalAuthenticator.create_system_users = True
 
-###
-設定多久刪除環境
-###
+
+# 設定多久刪除環境
+
 #shutdown the server after no activity for an hour
 c.ServerApp.shutdown_no_activity_timeout = 60 * 60
 #shutdown kernels after no activity for 30 minutes
@@ -39,9 +39,9 @@ c.MappingKernelManager.cull_interval = 2 * 60
 
 
 
-###
-設定建立新使用者的家目錄，並且與docker外的本機目錄連結，家目錄的owner ID/group ID需要與本機的user ID/group ID相同，否則會發生無法編輯的錯誤。
-###
+
+# 設定建立新使用者的家目錄，並且與docker外的本機目錄連結，家目錄的owner ID/group ID需要與本機的user ID/group ID相同，否則會發生無法編輯的錯誤。
+
 def create_dir_hook(spawner):
    """ Create directory """
    username = spawner.user.name  # get the username
@@ -55,8 +55,8 @@ notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR')
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = {'./jupyterhub-data/{username}': notebook_dir}
 
-###
-JupyterHub的設定與使用者資料會存在/persist下，以免JupyterHub重啟後全部消失。
-###
+
+# JupyterHub的設定與使用者資料會存在/persist下，以免JupyterHub重啟後全部消失。
+
 c.JupyterHub.cookie_secret_file = '/persist/jupyterhub_cookie_secret'
 c.JupyterHub.db_url = '/persist/jupyterhub.sqlite'
