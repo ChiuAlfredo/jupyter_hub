d1d5a725cf605d08b468bc30b5ead68e4255d9cf
alfredochiu <petercy32@gmail.com>
2024-02-29 11:57:37 +0800

    nw

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -50,7 +50,7 @@ c.DockerSpawner.debug = True
 
 # User containers will access hub by container name on the Docker network
 c.JupyterHub.hub_ip = "jupyterhub"
-c.JupyterHub.hub_port = 8000
+c.JupyterHub.hub_port = 8080
 
 # Persist hub data on volume mounted inside container
 c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
