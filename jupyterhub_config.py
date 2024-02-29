2042521354e2a514552693808e64eb973f85086a
alfredochiu <petercy32@gmail.com>
2024-03-01 03:12:11 +0800

    new

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,11 +14,11 @@ c = get_config()  # noqa: F821
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 
 # Spawn containers from this image
-# c.DockerSpawner.image = os.environ["DOCKER_NOTEBOOK_IMAGE"]
+c.DockerSpawner.image = os.environ["DOCKER_NOTEBOOK_IMAGE"]
 
-c.DockerSpawner.allowed_images = {'tensorflow' :os.environ["TENSORFLOW_NOTEBOOK_IMAGE"],
-                                  'pytorch': os.environ["PYTORCH_NOTEBOOK_IMAGE"],
-                                }
+# c.DockerSpawner.allowed_images = {'tensorflow' :os.environ["TENSORFLOW_NOTEBOOK_IMAGE"],
+#                                   'pytorch': os.environ["PYTORCH_NOTEBOOK_IMAGE"],
+#                                 }
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.extra_host_config = {'runtime': 'nvidia'}
 c.Spawner.environment = {'GRANT_SUDO': 'yes'}
