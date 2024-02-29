5c615af78582f6048ccc04e7ac6e2d3d705c1477
alfredochiu <petercy32@gmail.com>
2024-02-29 11:37:11 +0800

    new

--- pytorch-notebook /start.sh
+++ pytorch-notebook /start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 这里可以包含其他需要在容器启动时执行的命令
 # 启动Jupyter Lab，假设你已经安装了Jupyter Lab
-jupyter lab --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+jupyter lab --ip='0.0.0.0' --port=7777 --no-browser --allow-root
