76fc578642dec3c4683c6088d736e9962acae5be
alfredochiu <petercy32@gmail.com>
2024-02-29 21:07:25 +0800

    new

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,7 +14,7 @@ c = get_config()  # noqa: F821
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 
 # Spawn containers from this image
-c.DockerSpawner.image = os.environ["PYTORCH_NOTEBOOK_IMAGE"]
+c.DockerSpawner.image = os.environ["TENSORFLOW_NOTEBOOK_IMAGE"]
 
 # c.DockerSpawner.allowed_images = {
 #      "tensorflow-gpu": os.environ["TENSORFLOW_NOTEBOOK_IMAGE"],
